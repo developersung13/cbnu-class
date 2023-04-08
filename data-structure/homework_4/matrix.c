fddc1f11cdeb9d13a7df473e5a56a97b5b02b694
developersung13 <developersung13@gmail.com>
2023-04-08 15:01:46 +0900

    [자료구조] Homework_#4 matrix.c 코드 파일 내 (행렬 A + B) 연산 수행 addition_matrix함수 추가

--- data-structure/homework_4/matrix.c
+++ data-structure/homework_4/matrix.c
@@ -69,6 +69,26 @@ void print_matrix(int **matrixA, int **matrixB, int **matrixResult, char op) {
     free_matrix(matrixResult);
 }
 
+// 행렬 A + B 연산을 수행하는 함수
+void addition_matrix(int **matrixA, int **matrixB) {
+    int k, z; // for문의 인덱스 변수로써 사용될 변수 k, z 선언
+    
+    // 행렬 A + B한 연산 결괏값을 저장하는 용도로 사용될 행렬 result선언
+    // 행렬 result의 행에 대한 동적 메모리 할당
+    int **matrixResult = (int**)malloc(sizeof(int *) * matrixA_row);
+    // 행렬 result의 열에 대한 동적 메모리 할당
+    for (k=0; k < matrixA_row; k++)
+        matrixResult[k] = (int*)malloc(sizeof(int) * matrixA_column);
+        
+    // 행렬 A, B의 값을 각각 합한 값을 결과 행렬에 저장
+    for (k=0; k < matrixA_row; k++) // matrixA_row번 만큼 반복
+        for (z=0; z < matrixA_column; z++) // matrixA_column번 만큼 반복
+            matrixResult[k][z] = matrixA[k][z] + matrixB[k][z];
+    
+    // 행렬 A, B와 A + B된 결괏값을 출력하는 print_matrix함수 호출
+    print_matrix(matrixA, matrixB, matrixResult, '+');
+}
+
 // int형 데이터를 반환하는 main 함수 정의
 int main() {
     srand(time(NULL)); // 난수 생성 시 초기 시드 값을 설정
@@ -103,6 +123,9 @@ int main() {
             matrixA[k][z] = rand() % 9 + 1;
             matrixB[k][z] = rand() % 9 + 1;
         }
+
+    // 행렬 A + B 연산을 수행하는 함수 호출
+    addition_matrix(matrixA, matrixB);
 
     // matrixA 변수(=행렬 A) 동적 메모리 할당 해제
     free_matrix(matrixA);
