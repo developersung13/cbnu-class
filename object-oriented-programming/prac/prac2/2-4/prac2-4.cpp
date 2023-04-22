614059772a1fc8df8f65f80df9d283ca1d9ae701
developersung13 <developersung13@gmail.com>
2023-04-22 22:15:39 +0900

    [객체지향 프로그래밍] 03주 차 실습(4) 코드 refactoring

--- object-oriented-programming/prac/prac2/2-4/prac2-4.cpp
+++ object-oriented-programming/prac/prac2/2-4/prac2-4.cpp
@@ -1,30 +1,27 @@
-#include <iostream>
 #include <cstring>
+#include <iostream>
 using namespace std;
 
-char& find(char a[], char c, bool& success) {
-    int k;
-    success = false;
-    for (k=0; k < strlen(a); k++)
-        if (a[k] == c) {
-            success = true;
-            return a[k];
-        }
-    return a[k];
+char &find(char s[], char key, bool &b) {
+  int k;
+  for (k = 0; k < strlen(s); k++)
+    if (s[k] == key) {
+      b = true;
+      break;
+    }
+  return s[k];
 }
 
 int main() {
-    char s[] = "Mike";
-    bool b = false;
-    char& loc = find(s, 'M', b);
-    
-    if(b == false) {
-        cout << "M을 발견할 수 없다" << endl;
-        return 0;
-    }
-    
-    loc = 'm'; // 'M' 위치에 'm' 기록
-    cout << s << endl; // "mike"가 출력됨
-    
+  char s[] = "Mike";
+  bool b = false;
+  char &loc = find(s, 'M', b);
+  
+  if (b == false) {
+    cout << "M을 발견할 수 없다" << endl;
     return 0;
+  }
+  
+  loc = 'm';         // 'M' 위치에 'm' 기록
+  cout << s << endl; // "mike"가 출력됨
 }
