b7bfb380e2759223ea0c726c37a8ecc2fadafb97
developersung13 <developersung13@gmail.com>
2023-04-23 14:21:54 +0900

    [객체지향 프로그래밍] 05주 차 실습(1) 코드 수정

--- object-oriented-programming/prac/prac4/4-1/prac4-1.cpp
+++ object-oriented-programming/prac/prac4/4-1/prac4-1.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <string>
 using namespace std;
+#define MAX_LEN 5
 
 int main(int argc, const char* argv[]) {
     ios_base::sync_with_stdio(false);
     
-    string name, maxName;
+    string names[MAX_LEN], maxName;
     
-    for(int k=0; k < 5; k++) {
-        cout << "이름<<";
-        getline(cin, name);
-        maxName = maxName < name ? name : maxName;
+    for(int k=0; k < MAX_LEN; k++) {
+        cout << "이름 << ";
+        getline(cin, names[k]);
+        maxName = maxName < names[k] ? names[k] : maxName;
     } cout << "사전에서 가장 뒤에 나오는 문자열은 " << maxName;
     
     return 0;
